7f4e5c3abf69e3ec1c0d3df027ab990ab45143e8
22rmisri <22rmisri@athenian.org>
2021-02-07 08:52:31 +0000

    wrote code for first two methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,33 +1,66 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyMain {
-
     // Wrapper method
     public static boolean isSorted(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return false;
+        return isSortedRec(list, 0);
     }
 
     // This recursive method checks if the array is sorted in 
     // non-decreasing order
     public static boolean isSortedRec(ArrayList<Integer> list, int i) {
-        // YOUR CODE HERE
-        return false;
+        if (list.size()-1 == i) {
+            return true;
+        }
+        else if (isSortedRec(list, i + 1)) {
+            if (list.get(i) <= list.get(i+1)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
     }
 
 
 
      // Wrapper method
     public static boolean hasCountCopies(int[] arr, int x, int count) {
-        // YOUR CODE HERE
-        return false;
+        return hasCountCopiesRec(arr, x, count, 0);
     }
 
     // This recursive method checks if the array contains exactly
     // count copies of the integer x
     public static boolean hasCountCopiesRec(int[] arr, int x, int count, int i) {
-        // YOUR CODE HERE
-        return false;
+        if ((arr.length == i) && (count == 0)) {
+            return true;
+        }
+        else if ((arr.length == i) && (count != 0)) {
+            return false;
+        }
+        else if (arr[i] == x) {
+            if (hasCountCopiesRec(arr, x, count-1, i+1)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (arr[i] != x) {
+            if (hasCountCopiesRec(arr, x, count, i+1)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
     }
 
 
@@ -41,11 +74,14 @@ public class MyMain {
 
     // This recursive method calls binary search on the array
     public static boolean binarySearchRec(int[] arr, int num, int lowerBound, int upperBound) {        
-        // YOUR CODE HERE
-        return false;
+        if (lowerBound > upperBound) {
+            return false;
+        }
+        else if ()
     }
 
     public static void main(String[] args) {
-        // You can test your code here
+        ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(new Integer[] {1, 2, 3, 4}));
+        System.out.println(isSorted(list1));
     }
 }
